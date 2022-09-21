1f344103a15fc54dee55b8c3d2d50a70ae7644c1
Wei Lun <garyganweilun@gmail.com>
2022-09-22 01:35:06 +0800

    Added header

---
+++ src/components/MyHead.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+
+const MyHead:NextPage = () => {
+    return (
+        <Head>
+            <title>WTF | The Ultimate NFT Tracker</title>
+            <meta name="description" content="What The Floor | The Ultimate NFT Tracker" />
+            <meta name="author" content="Shoreasg" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    )
+}
+
+export default MyHead
