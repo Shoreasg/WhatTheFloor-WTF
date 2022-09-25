d4cf7d9514403c426c06446d8a2627d58a089161
Wei Lun <garyganweilun@gmail.com>
2022-09-25 15:26:03 +0800

    Added update api

--- src/server/router/getArkyCollections.ts
+++ src/server/router/getArkyCollections.ts
@@ -5,31 +5,32 @@ import axios from 'axios';
 
 export const getArkyCollectionsRouter = createRouter()
     .query("mapArkyCollections", {
+        //
         async resolve({ ctx }) {
             const { data } = await axios.get(env.ARKY_URL);
-            // console.log(data.result.entries);
-            const mapData = data.result.entries.map(async(data: any) => {
-              await ctx.prisma.arkyCollections.createMany(
+            data.result.entries.map(async (data: any) => {
+                await ctx.prisma.arkyCollections.createMany(
                     {
-                        data:[
-                            {   collectionsName: data.name,
-                                description: (data.description? data.description: "NA"),
+                        data: [
+                            {
+                                collectionsName: data.name,
+                                description: (data.description ? data.description : "NA"),
                                 tokenAddress: toBech32Address(data.address),
-                                websiteUrl: (data.websiteUrl? data.websiteUrl: "NA"),
-                                discordUrl: (data.discordUrl? data.discordUrl : "NA"),
-                                telegramUrl: (data.telegramUrl? data.telegramUrl : "NA"),
-                                twitterUrl: (data.twitterUrl? data.twitterUrl: "NA"),
-                                instagramUrl: (data.instagramUrl? data.instagramUrl: "NA"),
-                                profileImageUrl: (data.profileImageUrl? data.profileImageUrl : "NA"),
+                                websiteUrl: (data.websiteUrl ? data.websiteUrl : "NA"),
+                                discordUrl: (data.discordUrl ? data.discordUrl : "NA"),
+                                telegramUrl: (data.telegramUrl ? data.telegramUrl : "NA"),
+                                twitterUrl: (data.twitterUrl ? data.twitterUrl : "NA"),
+                                instagramUrl: (data.instagramUrl ? data.instagramUrl : "NA"),
+                                profileImageUrl: (data.profileImageUrl ? data.profileImageUrl : "NA"),
                                 floorPrice: data.priceStat.floorPrice,
                                 allTimeVolume: data.priceStat.allTimeVolume,
                                 totalToken: data.tokenStat.tokenCount,
                                 numHolders: data.tokenStat.holderCount,
-                                isVerified: (data.verifiedAt ? true: false),
-                                isScam: (data.reportLevel === 1? true : false),
+                                isVerified: (data.verifiedAt ? true : false),
+                                isScam: (data.reportLevel === 1 ? true : false),
                                 marketCap: "0",
                                 volume: data.priceStat.allTimeVolume
-                                
+
                             }
                         ]
 
@@ -37,7 +38,50 @@ export const getArkyCollectionsRouter = createRouter()
                 )
             })
 
-            return {success: true};
+            return { success: true };
+        },
+    }).query("updateArkyCollections", {
+        async resolve({ ctx }) {
+            const { data } = await axios.get(env.ARKY_URL);
+            data.result.entries.map(async (data: any) => {
+                await ctx.prisma.arkyCollections.upsert(
+                    {
+                        where: {
+                            collectionsName: data.name,
+                        },
+                        update: {
+                            floorPrice: data.priceStat.floorPrice,
+                            allTimeVolume: data.priceStat.allTimeVolume,
+                            isVerified: (data.verifiedAt ? true : false),
+                            isScam: (data.reportLevel === 1 ? true : false),
+                            marketCap: "0",
+                            volume: data.priceStat.allTimeVolume
+                        },
+                        create:
+                        {
+                            collectionsName: data.name,
+                            description: (data.description ? data.description : "NA"),
+                            tokenAddress: toBech32Address(data.address),
+                            websiteUrl: (data.websiteUrl ? data.websiteUrl : "NA"),
+                            discordUrl: (data.discordUrl ? data.discordUrl : "NA"),
+                            telegramUrl: (data.telegramUrl ? data.telegramUrl : "NA"),
+                            twitterUrl: (data.twitterUrl ? data.twitterUrl : "NA"),
+                            instagramUrl: (data.instagramUrl ? data.instagramUrl : "NA"),
+                            profileImageUrl: (data.profileImageUrl ? data.profileImageUrl : "NA"),
+                            floorPrice: data.priceStat.floorPrice,
+                            allTimeVolume: data.priceStat.allTimeVolume,
+                            totalToken: data.tokenStat.tokenCount,
+                            numHolders: data.tokenStat.holderCount,
+                            isVerified: (data.verifiedAt ? true : false),
+                            isScam: (data.reportLevel === 1 ? true : false),
+                            marketCap: "0",
+                            volume: data.priceStat.allTimeVolume
+                        },
+                    }
+                )
+
+            })
+            return { success: true };
         },
     })
     .query("getAllCollections", {
