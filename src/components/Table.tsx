b362a88c743b81f9ec546e729b3025637cb0ee5f
Wei Lun <garyganweilun@gmail.com>
2022-09-29 20:12:36 +0800

    Added loading while table is loading

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,8 +6,8 @@ export default function Table(arkyCollections: any) {
 
     const allTimeVolumeBN = new BigNumber(data.allTimeVolume).shiftedBy(-12);
     const sevenDayVolumeBN = new BigNumber(data.sevenDayVolume).shiftedBy(-12);
-   
-    
+
+
 
     return (
       <tr key={data.id}>
@@ -48,35 +48,55 @@ export default function Table(arkyCollections: any) {
           </p>
         </div>
       </div>
-      <div className="mt-8 flex flex-col">
-        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                      Collection Name
-                    </th>
-                    <th scope="col" className="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">
-                      Verified by Arky
-                    </th>
-                    <th scope="col" className="px-3 py-3.5 text-sm font-semibold text-gray-900 text-center">
-                      7 day Volume
-                    </th>
-                    <th scope="col" className="px-3 py-3.5 text-sm font-semibold text-gray-900 text-center">
-                      All Time Volume
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white">
-                {mapArkyCollections}
-                </tbody>
-              </table>
+      {mapArkyCollections ?
+        <div className="mt-8 flex flex-col">
+          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                        Collection Name
+                      </th>
+                      <th scope="col" className="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">
+                        Verified by Arky
+                      </th>
+                      <th scope="col" className="px-3 py-3.5 text-sm font-semibold text-gray-900 text-center">
+                        7 day Volume
+                      </th>
+                      <th scope="col" className="px-3 py-3.5 text-sm font-semibold text-gray-900 text-center">
+                        All Time Volume
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 bg-white">
+                    {mapArkyCollections}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </div> : <div className="mt-8 flex flex-col">
+          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-300 animate-pulse">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th>
+                        Loading
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 bg-white">
+                    {mapArkyCollections}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>}
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,23 +8,23 @@ import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
   // trpc.useQuery(["getArky.updateArkyCollections"],{staleTime: 3600000});
-  const arkyCollections =  trpc.useQuery(["getArky.getAllCollections"]);
+  const arkyCollections = trpc.useQuery(["getArky.getAllCollections"]);
 
-  const sortArkyCollections = arkyCollections.data?.sort((b:any,a:any)=>
-  {
+  const sortArkyCollections = arkyCollections.data?.sort((b: any, a: any) => {
     return a.allTimeVolume - b.allTimeVolume
   })
-  
+
 
   return (
     <Body>
       <CarouselBanner />
-        <div className="flex justify-center">
-          <h2 className="text-3xl font-bold leading-7 text-gray-900 sm:truncate sm:text-5xl sm:tracking-tight">
-           NFT Collections
-          </h2>
-        </div>
-      <Table arkyCollections={sortArkyCollections}/>
+      <div className="flex justify-center">
+        <h2 className="text-3xl font-bold leading-7 text-gray-900 sm:truncate sm:text-5xl sm:tracking-tight">
+          NFT Collections
+        </h2>
+      </div>
+      {arkyCollections.isLoading ? <Table /> : <Table arkyCollections={sortArkyCollections} />}
+
     </Body>
   );
 };
